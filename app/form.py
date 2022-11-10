937af5dcf6365ea4435982d9e562030690f02c1c
CaioSix <caio-cavalcanti@hotmail.com>
2022-11-09 21:34:20 -0300

    first push

---
+++ app/form.py
@@ -0,0 +1,10 @@
+from django.forms import ModelForm
+from app.models import Carro
+
+# Create the form class.
+
+
+class CarrosForm(ModelForm):
+    class Meta:
+        model = Carro
+        fields = ['modelo', 'marca', 'ano']
